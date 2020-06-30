19f2375c17b16296ba6af59ab0844dc57ea7fd43
Pete Roden <peroden@microsoft.com>
2020-06-29 22:51:28 -0400

    connect blob storage

--- api/getProfile/index.ts
+++ api/getProfile/index.ts
@@ -1,19 +1,30 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { BlobServiceClient, BlobDownloadResponseModel } from "@azure/storage-blob"
+
+interface UserInfo {
+    userId: string;
+    aboutMe: string;
+    interests: string;
+    skills: string;
+}
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = req.params.userId;
+    const userId = req.params.userId;
 
-    if (name) {
+    if (userId) {
+        const STORAGE_CONNECTION_STRING = process.env.STORAGE_CONNECTION_STRING || "";
+        const blobServiceClient = BlobServiceClient.fromConnectionString(STORAGE_CONNECTION_STRING);
+        const containerClient = blobServiceClient.getContainerClient("profiles");
+
+        const blockBlobClient = containerClient.getBlockBlobClient(userId + ".json");
+        const downloadBlockBlobResponse: BlobDownloadResponseModel = await blockBlobClient.download(0);
+        const userInfoString = await streamToString(downloadBlockBlobResponse.readableStreamBody!);
+        const userInfo: UserInfo = JSON.parse(userInfoString);
+
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body:
-                { 
-                    "userId": req.params.userId,
-                    "aboutMe": "I'm me",
-                    "interests": "interesting stuff",
-                    "skills": "lots and lots of skills"
-                }
+            body: userInfo
         };
     }
     else {
@@ -23,5 +34,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         };
     }
 };
+
+// A helper method used to read a Node.js readable stream into string
+async function streamToString(readableStream: NodeJS.ReadableStream): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const chunks: string[] = [];
+        readableStream.on("data", (data) => {
+            chunks.push(data.toString());
+        });
+        readableStream.on("end", () => {
+            resolve(chunks.join(""));
+        });
+        readableStream.on("error", reject);
+    });
+}
 
 export default httpTrigger;
