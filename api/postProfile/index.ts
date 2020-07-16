c511e888e06aa079da2a775746ec2279237cae03
Pete Roden <peroden@microsoft.com>
2020-07-15 23:03:57 -0400

    more linter issues

--- api/postProfile/index.ts
+++ api/postProfile/index.ts
@@ -1,23 +1,22 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { UserInfo } from "../models/UserInfo";
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { UserInfo } from '../models/UserInfo'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const userInfo: UserInfo = <UserInfo>context.req?.body;
+  const userInfo = context.req?.body as UserInfo
 
-    if (userInfo && userInfo.userId == context.req?.params.userId) {
-        context.bindings.userInfo = userInfo;
+  if (userInfo && userInfo.userId === context.req?.params.userId) {
+    context.bindings.userInfo = userInfo
 
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: `Saved profile for ${userInfo.userId}`
-        };
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: `Saved profile for ${userInfo.userId}`
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Invalid user info"
-        };
+  } else {
+    context.res = {
+      status: 400,
+      body: 'Invalid user info'
     }
-};
+  }
+}
 
-export default httpTrigger;
+export default httpTrigger
