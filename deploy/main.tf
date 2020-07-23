ebd76fd813b6dd399d94f888e6e9e3418d5602cc
Pete Roden <peroden@microsoft.com>
2020-07-22 21:01:36 -0400

    fixed typo

--- deploy/main.tf
+++ deploy/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_app_service_plan" "static_site" {
   name                = "${local.prefix}-app-service-plan"
   location            = var.location
   resource_group_name = azurerm_resource_group.static_site.name
-and  kind                = "Linux"
+  kind                = "Linux"
   reserved            = true
 
   sku {
